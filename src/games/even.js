ac96bb58119e622bad223792897e66f7aa0bfc32
Dmitry Iovev <iovlev.d@gmail.com>
2022-07-23 12:03:22 +0300

    Function refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -3,12 +3,7 @@ import init from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => {
-  if (number % 2 === 0) {
-    return true;
-  }
-  return false;
-};
+const isEven = (number) => (number % 2 === 0);
 
 const minNumber = 1;
 const maxNumber = 100;
